fb20e8ea2e750c021ef90f9d982dbd7dba4d3982
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-26 13:03:33 +0800

    Update list_sales.php

--- list_sales.php
+++ list_sales.php
@@ -1,12 +1,20 @@
 <?php
 	include 'dbconnect.php';
-	$search = (isset($_POST['search']) && $_POST['search'] != '') ? $_POST['search'] : '';
+	$search1 = (isset($_POST['search1']) && $_POST['search1'] != '') ? $_POST['search1'] : '';
+	$search2 = (isset($_POST['search2']) && $_POST['search2'] != '') ? $_POST['search2'] : '';
+
+	$total_payment = 0;
 
 
-	if(isset($search))
-		$sql = "SELECT * FROM tbl_customer WHERE cus_LName like '%$search%' OR cus_FName like '%$search%'";
-	else
-		$sql = "SELECT * FROM tbl_customer";
+	if(isset($search1))
+		{
+			if(isset($search2))
+				$sql = "SELECT * FROM tbl_orderline WHERE order_id IN (SELECT order_id FROM tbl_order WHERE order_date BETWEEN '$search1' AND '$search2')";
+		}
+	else{
+		$sql = "SELECT * FROM tbl_orderline ORDER BY orderline_id ASC";
+	}
+
 
 	$q = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 ?>
@@ -17,43 +25,58 @@
 </br>
 
 <form method = "POST" action="index.php?page=Sales">
-	<input class="search" type="text" name ="search">
+	<input class="search1" type="date" name ="search1"> 
+	<input class="search2" type="date" name ="search2">
 	<input class="submit" type="submit" name="submit" value="search">
 </form>
 
 </br>
 </br>
 
-	<table class="table" style="width: 100%;">
+	<table class="table">
 		<tr>
-			<td> Product Name </td>
-			<td> Quantity </td>
-			<td> Cost </td>
+			<th> CUSTOMER </th>
+			<th> PRODUCT NAME </th>
+			<th> QUANTITY </th>
+			<th> COST </th>
 		</tr>
 <?php 
 	while($row=mysqli_fetch_assoc($q))
 	{
+		$total_payment = $row['total_cost'] + $total_payment;
 ?>
 		<tr>
-			<td> <?php echo $row['cus_LName']. ", " . $row['cus_FName']; ?> </td>
-			<td> <?php echo $row['cus_age']; ?></td>
-			<td> <?php echo $row['cus_contact']; ?> </td>
-			<td> <?php echo $row['cus_address']; ?> </td>
 			<td> 
-				<a href = "index.php?page=Users&action=delete_customer&id=<?php echo $row['cus_id'];?>&lname=<?php echo $row['cus_LName'];?>"> Delete </a> 
+				<?php
+					$order_id = $row['order_id'];
+					$sql_order = "SELECT * FROM tbl_order WHERE order_id like '$order_id'";
+					$q_order = mysqli_query($conn, $sql_order) or die (mysqli_error($conn));
+					$o_fetch = mysqli_fetch_assoc($q_order);
+
+					$cus_id = $o_fetch['cus_id'];
+					$sql_customer = "SELECT * FROM tbl_customer WHERE cus_id like '$cus_id'";
+					$q_customer = mysqli_query($conn, $sql_customer) or die (mysqli_error($conn));
+					$c_fetch = mysqli_fetch_assoc($q_customer);
+
+					echo $c_fetch['cus_FName'].' '.$c_fetch['cus_LName'];
+				?>
 			</td>
-			<td> 
-				<a href = "index.php?page=Users&action=update_customer&id=<?php echo $row['cus_id'];?>"> Edit </a> 
-			</td>
-			<td> 
-				<a href = "processorder.php?id=<?php echo $row['cus_id'];?>"> Buy </a> 
-			</td>
+			<td> <?php echo $row['con_name']; ?></td>
+			<td> <?php echo $row['quantity']; ?> </td>
+			<td> <?php echo $row['total_cost']; ?> </td>
 		</tr>
 <?php
 	}
 ?>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><label for="payment"><b id="labeladd">Total Sales: </b></label></td>
+			<td><em id="text1">Php <?php echo $total_payment;?>.00</em></td>
+		</tr>
 	</table>
 	<br>
 	<br>
-<a href="index.php?page=Users&action=add_customer"> + Add another customer</a>
+	<br>
+	<br>
 	
